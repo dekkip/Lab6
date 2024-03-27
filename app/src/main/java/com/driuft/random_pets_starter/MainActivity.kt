7af11a5e7c990625d18a64a5b540655ee9b29bb2
Deki Peltshog <dekipeltshogcodes@gmail.com>
2024-03-26 19:52:18 -0600

    yess

--- app/src/main/java/com/driuft/random_pets_starter/MainActivity.kt
+++ app/src/main/java/com/driuft/random_pets_starter/MainActivity.kt
@@ -11,20 +11,31 @@ import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var petList: MutableList<String>
+    private lateinit var rvPets: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        rvPets = findViewById(R.id.pet_list)
+        petList = mutableListOf()
         getDogImageURL()
     }
 
     private fun getDogImageURL() {
         val client = AsyncHttpClient()
 
-        client["https://dog.ceo/api/breeds/image/random", object : JsonHttpResponseHandler() {
+        client["https://dog.ceo/api/breeds/image/random/20", object : JsonHttpResponseHandler() {
             override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
                 Log.d("Dog Success", "$json")
+                val petImageArray = json.jsonObject.getJSONArray("message")
+                for (i in 0 until petImageArray.length()) {
+                    petList.add(petImageArray.getString(i))
+                }
+                val adapter = PetAdapter(petList)
+                rvPets.adapter = adapter
+                rvPets.layoutManager = LinearLayoutManager(this@MainActivity)
+                rvPets.addItemDecoration(DividerItemDecoration(this@MainActivity, LinearLayoutManager.VERTICAL))
             }
 
             override fun onFailure(
